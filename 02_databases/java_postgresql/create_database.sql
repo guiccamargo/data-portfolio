c159b776a42b0016dae400cb01eb45d16e8f35fb
Guilherme Camargo <120288318+guiccamargo@users.noreply.github.com>
2026-06-16 21:21:07 -0300

    Delete 02_databases/java_postgresql directory

--- 02_databases/java_postgresql/create_database.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS produtos
-(
-    id serial NOT NULL,
-    nome character varying(50) NOT NULL,
-    preco decimal NOT NULL,
-    estoque integer NOT NULL,
-    CONSTRAINT produtos_pkey PRIMARY KEY (id)
-)
